3214a8e6644344be782edbed3514159ab5d2e91d
Roman Osadchiy <rosadchiy@yandex.ru>
2025-04-02 15:51:28 +0500

    Add firebasefile

---
+++ plugins/firebase.client.ts
@@ -0,0 +1,25 @@
+// plugins/firebase.client.ts
+import { defineNuxtPlugin } from "#app";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+export default defineNuxtPlugin(() => {
+  const firebaseConfig = {
+    apiKey: import.meta.env.API_KEY,
+    authDomain: import.meta.env.AUTH_DOMAIN,
+    projectId: import.meta.env.PROJECT_ID,
+    storageBucket: import.meta.env.STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+    appId: import.meta.env.APP_ID,
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  return {
+    provide: {
+      firebase: app,
+      db,
+    },
+  };
+});
